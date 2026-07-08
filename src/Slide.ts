65f53b3afa37c3112b137a3fe9d5368022131455
niusdev <vinidamascenadev@gmail.com>
2026-07-07 21:54:39 -0300

    feat: adicionar as 'thumbs'para acompanhar o progresso de tempo do slide atual

--- src/Slide.ts
+++ src/Slide.ts
@@ -10,6 +10,8 @@ export default class Slide {
   timeout: Timeout | null;
   pausedTimeout: Timeout | null;
   paused;
+  thumbItems: HTMLElement[] | null;
+  thumb: HTMLElement | null;
   constructor(
     container: Element,
     slides: Element[],
@@ -23,10 +25,16 @@ export default class Slide {
 
     this.timeout = null;
     this.pausedTimeout = null;
-    this.index = localStorage.getItem("activeSlide") ? Number(localStorage.getItem("activeSlide")) : 0;
+    this.index = localStorage.getItem("activeSlide")
+      ? Number(localStorage.getItem("activeSlide"))
+      : 0;
     this.slide = this.slides[this.index];
 
     this.paused = false;
+
+    this.thumbItems = null;
+    this.thumb = null;
+
     this.init();
   }
   hide(el: Element) {
@@ -40,7 +48,13 @@ export default class Slide {
     this.index = index;
     this.slide = this.slides[this.index];
     localStorage.setItem("activeSlide", String(this.index));
-     
+
+    if (this.thumbItems) {
+      this.thumb = this.thumbItems[this.index];
+      this.thumbItems.forEach((el) => el.classList.remove("active"));
+      this.thumb.classList.add("active");
+    }
+
     this.slides.forEach((el) => this.hide(el));
     this.slide.classList.add("active");
 
@@ -64,6 +78,7 @@ export default class Slide {
   auto(time: number) {
     this.timeout?.clear();
     this.timeout = new Timeout(() => this.next(), time);
+    if (this.thumb) this.thumb.style.animationDuration = `${time}ms`;
   }
 
   prev() {
@@ -82,6 +97,7 @@ export default class Slide {
     this.pausedTimeout = new Timeout(() => {
       this.timeout?.pause();
       this.paused = true;
+      this.thumb?.classList.add("paused");
       if (this.slide instanceof HTMLVideoElement) this.slide.pause();
     }, 300);
   }
@@ -91,6 +107,7 @@ export default class Slide {
     if (this.paused) {
       this.paused = false;
       this.timeout?.continue();
+      this.thumb?.classList.remove("paused");
       if (this.slide instanceof HTMLVideoElement) this.slide.play();
     }
   }
@@ -109,8 +126,20 @@ export default class Slide {
     prevButton.addEventListener("pointerup", () => this.prev());
     nextButton.addEventListener("pointerup", () => this.next());
   }
+
+  private addThumbItems() {
+    const thumbContainer = document.createElement("div");
+    thumbContainer.id = "slide-thumb";
+    for (let i = 0; i < this.slides.length; i++) {
+      thumbContainer.innerHTML += `<span><span class="thumb-item"></span></span>`;
+    }
+    this.controls.appendChild(thumbContainer);
+    this.thumbItems = Array.from(document.querySelectorAll(".thumb-item"));
+  }
+
   private init() {
     this.addControls();
+    this.addThumbItems();
     this.show(this.index);
   }
 }
